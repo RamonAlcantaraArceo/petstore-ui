9b6f99b1e67c41e2e503a0d3bae853cbd33c54b1
Ramon Alcantara Arceo <ramalc.ms@outlook.com>
2026-03-03 10:43:52 -0800

    chore: establish unit 10A lint-format baseline with bun and husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,50 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'storybook'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:storybook/recommended',
+    'prettier',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: ['dist/', 'coverage/', 'storybook-static/', 'node_modules/'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+  overrides: [
+    {
+      files: ['src/stories/**/*.tsx'],
+      rules: {
+        'react-hooks/rules-of-hooks': 'off',
+        'react/no-unescaped-entities': 'off',
+      },
+    },
+    {
+      files: ['src/testing/**/*.ts', 'src/testing/**/*.tsx'],
+      rules: {
+        '@typescript-eslint/no-namespace': 'off',
+      },
+    },
+  ],
+};
