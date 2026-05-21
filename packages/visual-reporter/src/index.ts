060028769fed0e4e900b3a081da24730ee18c45d
Ramon Alcantara Arceo <ramalc.ms@outlook.com>
2026-05-21 13:17:28 -0700

    chore(refactor): move visual-reporter-owned src files into packages/visual-reporter; update package entrypoint

--- packages/visual-reporter/src/index.ts
+++ packages/visual-reporter/src/index.ts
@@ -1,40 +1,40 @@
-export { VisualStatusBadge } from '../../../src/components/atoms/VisualStatusBadge';
-export type { VisualStatusBadgeProps } from '../../../src/components/atoms/VisualStatusBadge';
+export { VisualStatusBadge } from './components/atoms/VisualStatusBadge';
+export type { VisualStatusBadgeProps } from './components/atoms/VisualStatusBadge';
 
-export { VisualStatusChip } from '../../../src/components/atoms/VisualStatusChip';
-export type { VisualStatusChipProps } from '../../../src/components/atoms/VisualStatusChip';
+export { VisualStatusChip } from './components/atoms/VisualStatusChip';
+export type { VisualStatusChipProps } from './components/atoms/VisualStatusChip';
 
-export { VisualFilterButton } from '../../../src/components/atoms/VisualFilterButton';
-export type { VisualFilterButtonProps } from '../../../src/components/atoms/VisualFilterButton';
+export { VisualFilterButton } from './components/atoms/VisualFilterButton';
+export type { VisualFilterButtonProps } from './components/atoms/VisualFilterButton';
 
-export { VisualFilterBar } from '../../../src/components/molecules/VisualFilterBar';
-export type { VisualFilterBarProps } from '../../../src/components/molecules/VisualFilterBar';
+export { VisualFilterBar } from './components/molecules/VisualFilterBar';
+export type { VisualFilterBarProps } from './components/molecules/VisualFilterBar';
 
-export { VisualTreeLabel } from '../../../src/components/molecules/VisualTreeLabel';
-export type { VisualTreeLabelProps } from '../../../src/components/molecules/VisualTreeLabel';
+export { VisualTreeLabel } from './components/molecules/VisualTreeLabel';
+export type { VisualTreeLabelProps } from './components/molecules/VisualTreeLabel';
 
-export { VisualModeControls } from '../../../src/components/molecules/VisualModeControls';
-export type { VisualModeControlsProps } from '../../../src/components/molecules/VisualModeControls';
+export { VisualModeControls } from './components/molecules/VisualModeControls';
+export type { VisualModeControlsProps } from './components/molecules/VisualModeControls';
 
-export { VisualImagePanel } from '../../../src/components/molecules/VisualImagePanel';
-export type { VisualImagePanelProps } from '../../../src/components/molecules/VisualImagePanel';
+export { VisualImagePanel } from './components/molecules/VisualImagePanel';
+export type { VisualImagePanelProps } from './components/molecules/VisualImagePanel';
 
-export { VisualSliderCompare } from '../../../src/components/molecules/VisualSliderCompare';
-export type { VisualSliderCompareProps } from '../../../src/components/molecules/VisualSliderCompare';
+export { VisualSliderCompare } from './components/molecules/VisualSliderCompare';
+export type { VisualSliderCompareProps } from './components/molecules/VisualSliderCompare';
 
-export { VisualComparisonPanel } from '../../../src/components/molecules/VisualComparisonPanel';
-export type { VisualComparisonPanelProps } from '../../../src/components/molecules/VisualComparisonPanel';
+export { VisualComparisonPanel } from './components/molecules/VisualComparisonPanel';
+export type { VisualComparisonPanelProps } from './components/molecules/VisualComparisonPanel';
 
-export { VisualComponentTree } from '../../../src/components/organisms/VisualComponentTree';
-export type { VisualComponentTreeProps } from '../../../src/components/organisms/VisualComponentTree';
+export { VisualComponentTree } from './components/organisms/VisualComponentTree';
+export type { VisualComponentTreeProps } from './components/organisms/VisualComponentTree';
 
-export { VisualVariantCard } from '../../../src/components/organisms/VisualVariantCard';
-export type { VisualVariantCardProps } from '../../../src/components/organisms/VisualVariantCard';
+export { VisualVariantCard } from './components/organisms/VisualVariantCard';
+export type { VisualVariantCardProps } from './components/organisms/VisualVariantCard';
 
-export { VisualReportApp } from '../../../src/components/organisms/VisualReportApp';
-export type { VisualReportAppProps } from '../../../src/components/organisms/VisualReportApp';
+export { VisualReportApp } from './components/organisms/VisualReportApp';
+export type { VisualReportAppProps } from './components/organisms/VisualReportApp';
 
-export * from '../../../src/visual-report/types';
-export * from '../../../src/visual-report/constants';
-export * from '../../../src/visual-report/model';
-export * from '../../../src/visual-report/assets';
+export * from './visual-report/types';
+export * from './visual-report/constants';
+export * from './visual-report/model';
+export * from './visual-report/assets';
