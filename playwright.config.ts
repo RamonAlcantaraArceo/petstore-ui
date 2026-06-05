14c89bbf92907281ae9e0b1fb1beec671d01d358
RamonAlcantaraArceo <ramalc.ms@outlook.com>
2026-06-05 10:24:02 -0700

    chore: add dotenv dependency to improve config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const isCI = !!process.env.CI;
 const additionalViewportProjectsEnabled = process.env.PW_ENABLE_ADDITIONAL_VIEWPORTS === '1';
